c3badc17379a28f775d2fedf4d856bb076c98ce9
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-09-20 09:52:13 +0530

    Commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import Counter from "./Counter";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    return (
+        <div>
+            {/* Do not remove the main div */}
+            <Counter />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    function btnHandle()
+    {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <p onClick={() => {
+                setCount(0);
+            }}>Button clicked {count} times.</p>
+            <button onClick={btnHandle}>Click me</button>
+        </div>
+    )
+}
+
+export default Counter
